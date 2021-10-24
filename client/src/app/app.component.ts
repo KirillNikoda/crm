34c8340d5b7869dcdd5c36dfb0923195c5458814
KirillNikoda <nikodakirill386@gmail.com>
2021-10-24 16:12:46 +0300

    feat: angular-apollo setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  users: any[] = [];
+
+  constructor(private apollo: Apollo) {}
+
+  query() {
+    return gql`
+      {
+        users {
+          id
+          email
+          name
+        }
+      }
+    `;
+  }
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: this.query(),
+      })
+      .valueChanges.subscribe(({ data }: any) => (this.users = data.users));
+  }
 }
